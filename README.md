5fdc172134c41002d388cac58c4d7177fd384159
Aqudi <gjdigj@naver.com>
2020-04-30 12:56:45 +0900

    Add SnackBar example 2, 3

--- README.md
+++ README.md
@@ -42,7 +42,8 @@ Document link : [Display a snackbar](https://flutter.dev/docs/cookbook/design/sn
     }
     ```
     + **해결법**
-      Scaffold 를 Builder 위젯의 build 함수에 넣어줌으로서 부모에서 받은 context 외에 Builder 위젯부터 시작하는 새로운 context childContext 를 만들어 사용할 수 있다.
+      Scaffold 를 Builder 위젯의 build 함수에 넣어줌으로서 부모에서 받은 context 외에 Builder 
+      위젯부터 시작하는 새로운 context childContext 를 만들어 사용할 수 있다.
 
       ```dart
       body: Builder(builder: (BuildContext childContext) {
@@ -62,4 +63,33 @@ Document link : [Display a snackbar](https://flutter.dev/docs/cookbook/design/sn
           );
         }));
       ```
-2. 
+    + **해결법2**  
+    커스텀 Widget 을 만들어준다. 
+    이렇게 할 시 Widget 의 build 메소드를 오버라이드할 때 Context 를 새롭게 
+    만드므로 위의 Builder 위젯을 사용한 것과 동일한 효과가 난다.
+    ```dart
+    class MySnackBar extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Center(
+          child: RaisedButton(
+            child: Text('Show me'),
+            onPressed: () {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Snack Bar with custom widget!!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.teal,
+                duration: Duration(microseconds: 1000),
+              ));
+            },
+          ),
+        );
+      }
+    }
+    ```
+    + **해결법3**
+    Flutter Toast Library 사용 [링크](https://pub.dev/packages/fluttertoast)
+
--- lib/snack_bar_page.dart
+++ lib/snack_bar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 // Document: https://flutter.dev/docs/cookbook/design/snackbars
 class SnackBarPage extends StatefulWidget {
@@ -15,21 +16,77 @@ class _SnackBarPageState extends State<SnackBarPage> {
           centerTitle: true,
           backgroundColor: Colors.red,
         ),
-        body: Builder(builder: (BuildContext childContext) {
-          return Center(
-            child: FlatButton(
-              child: Text('Show me',
-                  style: TextStyle(
-                    color: Colors.white,
-                  )),
-              color: Colors.red,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // 1. SnackBar with Builder!!
+            Builder(builder: (BuildContext childContext) {
+              return Center(
+                child: FlatButton(
+                  child: Text('SnackBar with Builder widget',
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
+                  color: Colors.red,
+                  onPressed: () {
+                    Scaffold.of(childContext).showSnackBar(SnackBar(
+                      content: Text('This is SnackBar!!'),
+                      duration: Duration(microseconds: 500),
+                    ));
+                  },
+                ),
+              );
+            }),
+
+            // 2. Custom SnackBar widget
+            MySnackBar('Custom SnackBar widget'),
+
+            // 3. SnackBar with Flutter Toast library
+            FlatButton(
               onPressed: () {
-                Scaffold.of(childContext).showSnackBar(SnackBar(
-                  content: Text('This is SnackBar!!'),
-                ));
+                flutterToast();
               },
-            ),
-          );
-        }));
+              child: Text('Flutter Toast Library'),
+              color: Colors.blue,
+            )
+          ],
+        ));
   }
+}
+
+class MySnackBar extends StatelessWidget {
+  final String _title;
+
+  MySnackBar(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text(_title),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(
+              'Snack Bar with custom widget!!',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.teal,
+            duration: Duration(microseconds: 500),
+          ));
+        },
+      ),
+    );
+  }
+}
+
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: 'Flutter Toast',
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.redAccent,
+      fontSize: 20.0,
+      timeInSecForIosWeb: 1,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
 }
